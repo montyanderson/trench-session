14fa0735c5282cef83c44daa1c8d919c89c653bb
Monty Anderson <monty.linux@gmail.com>
2016-11-06 21:43:45 +0000

    init

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
 # trench-session
+
+Session middleware for [Trench](https://github.com/montyanderson/trench), using Redis.
+
+## Usage
+
+``` javascript
+const Trench = require("trench");
+const session = require("trench-session");
+
+const app = new Trench();
+
+app.use(session({
+	app: "app"
+}));
+
+app.get("/", (req, res) => {
+	if(!req.session.i) req.session.i = 0;
+	req.session.i++;
+
+	res.end(req.session.i.toString());
+});
+
+app.listen(8080);
+```
+
+## API
+
+### session([options])
+
+Returns a middleware function to generate functions, then save them when `res.end()` is called.
+
+#### options
+
+##### app
+
+Type: `string`
+
+A name for the application, used so you can store multiple app's sessions in the same redis database, without possibility of interference.
+
+##### db
+
+Type: `string`, `object`
+
+Connection configuration, to be passed to `redis.createClient()`.
+
+##### client
+
+Type: `object`
+
+A redis client, i.e. created by `redis.createClient()`.
---
+++ index.js
@@ -0,0 +1,48 @@
+const redis = require("p-redis");
+const cookie = require("cookie");
+const uid = require("uid-safe");
+
+function session(options = {}) {
+	const db = options.client || redis.createClient(options.db);
+
+	return (req, res) => {
+		let key;
+
+		return Promise.resolve().then(() => {
+			if(!req.headers.cookie) {
+				req.cookies = {};
+			} else {
+				req.cookies = cookie.parse(req.headers.cookie) || {};
+			}
+
+			if(!req.cookies.session) {
+				return uid(24).then(id => {
+					res.setHeader("Set-Cookie", cookie.serialize("session", id));
+					req.cookies.session = id;
+				});
+			}
+		}).then(() => {
+			key = "session:" + (options.app || "app") + ":" + req.cookies.session;
+
+			return db.existsAsync(key).then(exists => {
+				if(exists) {
+					return db.getAsync(key).then(json => {
+						req.session = JSON.parse(json);
+
+					});
+				} else {
+					req.session = {};
+				}
+			});
+		}).then(() => {
+			const _end = res.end;
+
+			res.end = function() {
+				db.setAsync(key, JSON.stringify(req.session));
+				_end.apply(this, arguments);
+			}
+		});
+	}
+};
+
+module.exports = session;
